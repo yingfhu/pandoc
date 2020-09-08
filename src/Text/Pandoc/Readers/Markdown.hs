60900fd65f55e88e619f3a6a4b288226224063fe
John MacFarlane <jgm@berkeley.edu>
2020-09-07 21:49:56 -0700

    Markdown reader: tweak abbreviation support.
    Don't insert a nonbreaking space after a potential
    abbreviation if it comes right before a note or citation.
    This messes up several things, including citeproc moving
    of note citations.

--- src/Text/Pandoc/Readers/Markdown.hs
+++ src/Text/Pandoc/Readers/Markdown.hs
@@ -1662,6 +1662,7 @@ str = do
       abbrevs <- getOption readerAbbreviations
       if not (T.null result) && T.last result == '.' && result `Set.member` abbrevs
          then try (do ils <- whitespace
+                      notFollowedBy (() <$ cite <|> () <$ note)
                       -- ?? lookAhead alphaNum
                       -- replace space after with nonbreaking space
                       -- if softbreak, move before abbrev if possible (#4635)
